4dea905768b1613bc4ea7eb33e8a436c1c19a29f
sravani <sandhyapyla.8@gmail.com>
2024-06-09 07:00:08 +0530

    done filter with payer

--- app/component/FilterModal.jsx
+++ app/component/FilterModal.jsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { LuFilter } from "react-icons/lu";
 
 export default function FilterModal({ data, setData, waitlistData }) {
+  console.log("waitlistDatea===============>", waitlistData);
   const [isopen, setIsOpen] = useState(false);
   const [activeTab, setActiveTab] = useState("scheduledDate");
   const [dateRange, setDateRange] = useState("All time");
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
+  const [searchPeople, setSearchPeople] = useState("");
+  const [searchPayer, setSearchPayer] = useState([]);
+  const [selectedPerson, setSelectedPerson] = useState([]);
 
   // filter with Secheduled Date----------------------------------
 
@@ -35,28 +39,43 @@ export default function FilterModal({ data, setData, waitlistData }) {
           });
         }
         break;
-        case "Last 30 days":
-          filteredData = filterByDateRange(waitlistData, currentDate, 30);
-          break;
-        case "This month":
-          filteredData = filterByMonth(waitlistData, currentDate);
-          break;
-        case "Last month":
-          filteredData = filterByMonth(waitlistData, new Date(currentDate.getFullYear(), currentDate.getMonth() - 1));
-          break;
-        case "This quarter":
-          filteredData = filterByQuarter(waitlistData, currentDate);
-          break;
-        case "2 quarter ago":
-          filteredData = filterByQuarter(waitlistData, new Date(currentDate.getFullYear(), currentDate.getMonth() - 6));
-          break;
-        case "This year":
-          filteredData = filterByYear(waitlistData, currentDate);
-          break;
-        case "Last year":
-          filteredData = filterByYear(waitlistData, new Date(currentDate.getFullYear() - 1, 0));
-          break;
-          default: filteredData=data
+      case "Last 30 days":
+        filteredData = filterByDateRange(waitlistData, currentDate, 30);
+        break;
+      case "This month":
+        filteredData = filterByMonth(waitlistData, currentDate);
+        break;
+      case "Last month":
+        filteredData = filterByMonth(
+          waitlistData,
+          new Date(currentDate.getFullYear(), currentDate.getMonth() - 1)
+        );
+        break;
+      case "This quarter":
+        filteredData = filterByQuarter(waitlistData, currentDate);
+        break;
+      case "2 quarter ago":
+        filteredData = filterByQuarter(
+          waitlistData,
+          new Date(currentDate.getFullYear(), currentDate.getMonth() - 6)
+        );
+        break;
+      case "This year":
+        filteredData = filterByYear(waitlistData, currentDate);
+        break;
+      case "Last year":
+        filteredData = filterByYear(
+          waitlistData,
+          new Date(currentDate.getFullYear() - 1, 0)
+        );
+        break;
+      default:
+        filteredData = data;
+    }
+    if (selectedPerson.length > 0) {
+      filteredData = filteredData.filter((data) =>
+        selectedPerson.some((item) => item.id === data.id)
+      );
     }
     setData(filteredData);
   };
@@ -66,7 +85,7 @@ export default function FilterModal({ data, setData, waitlistData }) {
       return (currentDate - itemDate) / (1000 * 60 * 60 * 24) <= days;
     });
   };
-  
+
   const filterByMonth = (waitlistData, currentDate) => {
     return waitlistData.filter((item) => {
       const itemDate = new Date(item.created_on);
@@ -76,7 +95,7 @@ export default function FilterModal({ data, setData, waitlistData }) {
       );
     });
   };
-  
+
   const filterByQuarter = (waitlistData, currentDate) => {
     const currentQuarter = Math.floor((currentDate.getMonth() + 3) / 3);
     return waitlistData.filter((item) => {
@@ -88,7 +107,7 @@ export default function FilterModal({ data, setData, waitlistData }) {
       );
     });
   };
-  
+
   const filterByYear = (waitlistData, currentDate) => {
     return waitlistData.filter((item) => {
       const itemDate = new Date(item.created_on);
@@ -96,19 +115,57 @@ export default function FilterModal({ data, setData, waitlistData }) {
     });
   };
   //  filter with people--------------------------------------
+  const handleSearch = (e) => {
+    const searchvalue = e.target.value;
+    setSearchPeople(searchvalue);
+  };
 
+  useEffect(() => {
+    if (searchPeople.length > 0) {
+      const filteredData = waitlistData
+        .filter((ele) =>
+          ele.payer.toLowerCase().startsWith(searchPeople.toLowerCase())
+        )
+        .slice(0, 10)
+        .sort((a, b) => a.payer.localeCompare(b.payer));
+      setSearchPayer(filteredData);
+    } else {
+      setSearchPayer([]);
+    }
+  }, [searchPeople]);
+
+  const handleSelectedPayer = (id) => {
+    const selectedPayer = waitlistData.find((p) => p.id === id);
+    if (selectedPayer) {
+      if (!selectedPerson.some((p) => p.id === id)) {
+        setSelectedPerson([...selectedPerson, selectedPayer]);
+      } else {
+        setSelectedPerson(selectedPerson.filter((person) => person.id !== id));
+      }
+    }
+  };
+  
+
+  const removePayer = (item) => {
+    setSelectedPerson(selectedPerson.filter((p) => p.id !== item));
+  };
+  const selectedResult = (id) =>
+    selectedPerson.some((person) => person.id === id);
   // filter with services and products-------------------------
 
   const applyDataFilter = () => {
-    filterTableData(dateRange);
+    filterTableData(dateRange, selectedPerson);
     setIsOpen(false);
   };
-  const resetDefaultFilter=()=>{
+  const resetDefaultFilter = () => {
     setIsOpen(false);
     setStartDate(null);
     setEndDate(null);
-    setDateRange("All time")
-  }
+    setDateRange("All time");
+    setSearchPayer([]);
+    setSearchPeople("");
+    setSelectedPerson([]);
+  };
 
   return (
     <>
@@ -119,133 +176,182 @@ export default function FilterModal({ data, setData, waitlistData }) {
         {isopen && (
           <div className="fixed inset-0 bg-opacity-30 bg-gray-900  flex justify-center items-center">
             <div className="w-3/4 p-6 bg-white rounded-lg shadow-lg max-w-4xl">
-            <div className="flex">
-              <div className="w-1/4 pr-4 border-r">
-                <button
-                  onClick={() => setActiveTab("scheduledDate")}
-                  className={`block w-full text-left px-4 py-2 ${
-                    activeTab === "scheduledDate" ? "bg-gray-200" : ""
-                  }`}
-                >
-                  Scheduled Date
-                </button>
-                <button
-                  onClick={() => setActiveTab("people")}
-                  className={`block w-full text-left px-4 py-2 ${
-                    activeTab === "people" ? "bg-gray-200" : ""
-                  }`}
-                >
-                  People
-                </button>
-                <button
-                  onClick={() => setActiveTab("servicesAndProducts")}
-                  className={`block w-full text-left px-4 py-2 ${
-                    activeTab === "servicesAndProducts" ? "bg-gray-200" : ""
-                  }`}
-                >
-                  Services / Products
-                </button>
-              </div>
-              <div className="w-3/4 pl-4">
-                {activeTab === "scheduledDate" && (
-                  <div>
-                    <label
-                      className="font-bold text-gray-700 block mb-2"
-                      htmlFor="timeRange"
-                    >
-                      Show orders for
-                    </label>
-                    <div className="relative mb-4">
-                      <select
-                        className="px-2 py-2 w-full border rounded"
-                        id="timeRange"
-                        style={{ maxHeight: "232px", overflowY: "auto" }}
-                        onChange={handleDateRange}
-                        value={dateRange}
+              <div className="flex">
+                <div className="w-1/4 pr-4 border-r">
+                  <button
+                    onClick={() => setActiveTab("scheduledDate")}
+                    className={`block w-full text-left px-4 py-2 ${
+                      activeTab === "scheduledDate" ? "bg-gray-200" : ""
+                    }`}
+                  >
+                    Scheduled Date
+                  </button>
+                  <button
+                    onClick={() => setActiveTab("people")}
+                    className={`block w-full text-left px-4 py-2 ${
+                      activeTab === "people" ? "bg-gray-200" : ""
+                    }`}
+                  >
+                    People
+                  </button>
+                  <button
+                    onClick={() => setActiveTab("servicesAndProducts")}
+                    className={`block w-full text-left px-4 py-2 ${
+                      activeTab === "servicesAndProducts" ? "bg-gray-200" : ""
+                    }`}
+                  >
+                    Services / Products
+                  </button>
+                </div>
+                <div className="w-3/4 pl-4">
+                  {activeTab === "scheduledDate" && (
+                    <div>
+                      <label
+                        className="font-bold text-gray-700 block mb-2"
+                        htmlFor="timeRange"
                       >
-                        <option value={"All time"}>All time</option>
-                        <option value={"Custom"}>Custom</option>
-                        <option value={"Last 30 days"}>Last 30 days</option>
-                        <option value={"This month"}>This month</option>
-                        <option value={"Last month"}>Last month</option>
-                        <option value={"This quarter"}>This quarter</option>
-                        <option value={"2 quarter ago"}>2 quarter ago</option>
-                        <option value={"This year"}>This year</option>
-                        <option value={"Last year"}>Last year</option>
-                      </select>
-                    </div>
-                    <div className="flex space-x-4">
-                      <div>
-                        <label
-                          className="text-gray-700 block font-bold mb-2"
-                          htmlFor="fromDate"
+                        Show orders for
+                      </label>
+                      <div className="relative mb-4">
+                        <select
+                          className="px-2 py-2 w-full border rounded"
+                          id="timeRange"
+                          style={{ maxHeight: "232px", overflowY: "auto" }}
+                          onChange={handleDateRange}
+                          value={dateRange}
                         >
-                          From
-                        </label>
-                        <DatePicker
-                          className="px-3 py-2 w-full border rounded"
-                          placeholderText="Pick the Date"
-                          onChange={(e)=>setStartDate(e)}
-                          selected={startDate}
-                        />
+                          <option value={"All time"}>All time</option>
+                          <option value={"Custom"}>Custom</option>
+                          <option value={"Last 30 days"}>Last 30 days</option>
+                          <option value={"This month"}>This month</option>
+                          <option value={"Last month"}>Last month</option>
+                          <option value={"This quarter"}>This quarter</option>
+                          <option value={"2 quarter ago"}>2 quarter ago</option>
+                          <option value={"This year"}>This year</option>
+                          <option value={"Last year"}>Last year</option>
+                        </select>
                       </div>
-                      <div>
-                        <label
-                          className="text-gray-700 block font-bold mb-2"
-                          htmlFor="toDate"
-                        >
-                          To
-                        </label>
-                        <DatePicker
-                          className="px-3 py-2 w-full border rounded"
-                          placeholderText="Pick the Date"
-                          selected={endDate}
-                          onChange={(e)=>setEndDate(e)}
-                        />
+                      <div className="flex space-x-4">
+                        <div>
+                          <label
+                            className="text-gray-700 block font-bold mb-2"
+                            htmlFor="fromDate"
+                          >
+                            From
+                          </label>
+                          <DatePicker
+                            className="px-3 py-2 w-full border rounded"
+                            placeholderText="Pick the Date"
+                            onChange={(e) => setStartDate(e)}
+                            selected={startDate}
+                          />
+                        </div>
+                        <div>
+                          <label
+                            className="text-gray-700 block font-bold mb-2"
+                            htmlFor="toDate"
+                          >
+                            To
+                          </label>
+                          <DatePicker
+                            className="px-3 py-2 w-full border rounded"
+                            placeholderText="Pick the Date"
+                            selected={endDate}
+                            onChange={(e) => setEndDate(e)}
+                          />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                )}
-                {activeTab === "people" && (
-                  <div>
-                    <input
-                      className="px-3 py-2 w-full border rounded mb-4"
-                      placeholder="Search for People or Client name"
-                    />
-                  </div>
-                )}
-                {activeTab === "servicesAndProducts" && (
-                  <div className="filter-options flex flex-row items-center">
-                    <input
-                      type="radio"
-                      id="search-by-name"
-                      name="search-option"
-                      className="mx-2"
-                    />
-                    <label htmlFor="search-by-name">Search by name</label>
-                    <input
-                      type="radio"
-                      id="search-by-tags"
-                      name="search-option"
-                      className="mx-2"
-                    />
-                    <label htmlFor="search-by-tags">Search by tags</label>
-                  </div>
-                )}
+                  )}
+                  {activeTab === "people" && (
+                    <div>
+                      <input
+                        type="text"
+                        className="px-3 py-2 w-full border rounded mb-4"
+                        placeholder="Search for People or Client name"
+                        onChange={handleSearch}
+                        id="peopleFilter"
+                        value={searchPeople}
+                      />
+
+                     <div>
+                     {selectedPerson.length > 0 && (
+                        <div>
+                          <h4>Selected :</h4>
+                          {selectedPerson.map((ele) => (
+                            <div key={ele.id}>
+                              <label>
+                                <input
+                                  type="checkbox"
+                                  onChange={() => removePayer(ele.id)}
+                                  checked={selectedResult(ele.id)}
+                                />
+                                {ele.payer}
+                              </label>
+                            </div>
+                          ))}
+                        </div>
+                      )}
+                     </div>
+                      {searchPeople.length > 0 && (
+                        <div>
+                          <h4>
+                            Showing {searchPayer.length} results matching "
+                            {searchPeople}":
+                          </h4>
+                          {searchPayer.map((p) => (
+                            <div key={p.id} className="m-0.5 flex flex-row">
+                              <label>
+                                <input
+                                  type="checkbox"
+                                  checked={selectedResult(p.id)}
+                                  onChange={() => handleSelectedPayer(p.id)}
+                                />
+                                {p.payer}
+                              </label>
+                              <div className="flex flex-row font-sans ml-2 bg-slate-100 text-black p-1 text-xs rounded-md"></div>
+                              <span>payer</span>
+                            </div>
+                          ))}
+                        </div>
+                      )}
+                    </div>
+                  )}
+                  {activeTab === "servicesAndProducts" && (
+                    <div className="filter-options flex flex-row items-center">
+                      <input
+                        type="radio"
+                        id="search-by-name"
+                        name="search-option"
+                        className="mx-2"
+                      />
+                      <label htmlFor="search-by-name">Search by name</label>
+                      <input
+                        type="radio"
+                        id="search-by-tags"
+                        name="search-option"
+                        className="mx-2"
+                      />
+                      <label htmlFor="search-by-tags">Search by tags</label>
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="flex justify-end mt-4">
+                <button
+                  className="px-4 py-2 bg-blue-500 text-white rounded mr-2"
+                  onClick={resetDefaultFilter}
+                >
+                  Reset to Default
+                </button>
+                <button
+                  className="px-4 py-2 bg-blue-500 text-white rounded"
+                  onClick={applyDataFilter}
+                >
+                  Apply
+                </button>
               </div>
             </div>
-            <div className="flex justify-end mt-4">
-              <button className="px-4 py-2 bg-blue-500 text-white rounded mr-2" onClick={resetDefaultFilter}>
-                Reset to Default
-              </button>
-              <button
-                className="px-4 py-2 bg-blue-500 text-white rounded"
-                onClick={applyDataFilter}
-              >
-                Apply
-              </button>
-            </div>
-          </div>
           </div>
         )}
       </div>
--- app/page.js
+++ app/page.js
@@ -6,61 +6,15 @@ import axios from 'axios';
 import SummaryBoxes from './component/SummaryBoxes';
 import FilterModal from './component/FilterModal';
 
-
-
-const filterData = (data, filters) => {
-  return data.filter(item => {
-    let isMatch = true;
-
-    if (filters.dateRange) {
-      // Apply date range filter logic here
-    }
-
-    if (filters.fromDate && filters.toDate) {
-      const itemDate = new Date(item.createdOn);
-      const fromDate = new Date(filters.fromDate);
-      const toDate = new Date(filters.toDate);
-      if (!(itemDate >= fromDate && itemDate <= toDate)) {
-        isMatch = false;
-      }
-    }
-
-    if (filters.searchTerm) {
-      const searchTerm = filters.searchTerm.toLowerCase();
-      if (!(item.payer.toLowerCase().includes(searchTerm) || item.attendee?.toLowerCase().includes(searchTerm))) {
-        isMatch = false;
-      }
-    }
-
-    if (filters.serviceSearchType && filters.serviceSearchValue) {
-      const searchValue = filters.serviceSearchValue.toLowerCase();
-      if (filters.serviceSearchType === 'name' && !item.services.toLowerCase().includes(searchValue)) {
-        isMatch = false;
-      }
-      if (filters.serviceSearchType === 'tags' && !item.serviceTags?.toLowerCase().includes(searchValue)) {
-        isMatch = false;
-      }
-    }
-
-    return isMatch;
-  });
-};
-
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [filters, setFilters] = useState({});
   const [data, setData] = useState([]);
    const [waitlistData,setWaitlistData]=useState([])
-  // const applyFilter = (filterValues) => {
-  //   const filteredData = filterData(initialData, filterValues);
-  //   setData(filteredData);
-  //   setFilters(filterValues);
-  // };
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(`https://backend-data-theta.vercel.app/data`);
+        const res = await axios.get(`https://backend-data-nine.vercel.app/data`);
         setData(res.data);
         setWaitlistData(res.data)
       } catch (error) {
@@ -85,9 +39,6 @@ export default function Home() {
          data={data}
          setData={setData}
          waitlistData={waitlistData}
-        // isOpen={isModalOpen}
-        // onClose={() => setIsModalOpen(false)}
-        // applyFilter={applyFilter}
       />
       <div className="h-[70%]">
         <Table data={data} />
